de7ea7851809b5e8516c4cca81fac08cfb593794
marcoskeiji <marcoskeiji@gmail.com>
2022-07-24 03:43:32 -0300

    operacoes com mutaveis

---
+++ src/main/kotlin/one/digitalinnovation/collection/TestColecoesMutable.kt
@@ -0,0 +1,31 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "CLT")
+    val maria = Funcionario("Maria", 4000.0, "PJ")
+
+    println("LIST")
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach { println(it) }
+    println("")
+
+    funcionarios.add(pedro)
+    funcionarios.forEach { println(it) }
+    println("")
+
+    funcionarios.remove(joao)
+    funcionarios.forEach { println(it) }
+    println("")
+
+    println("SET")
+    val funcionariosSet = mutableSetOf(joao)
+    funcionariosSet.forEach { println(it) }
+    println("")
+    funcionariosSet.add(pedro)
+    funcionariosSet.add(maria)
+    funcionariosSet.forEach { println(it) }
+    println("")
+    funcionariosSet.remove(maria)
+    funcionariosSet.forEach { println(it) }
+}
