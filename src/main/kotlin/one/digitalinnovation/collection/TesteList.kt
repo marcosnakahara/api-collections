ea014ebc7c0895dc024209796a6bb5f014860ab5
marcoskeiji <marcoskeiji@gmail.com>
2022-07-24 03:18:11 -0300

    operacoes com sets

--- src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -21,16 +21,3 @@ fun main() {
 
 
 }
-
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContratacao: String
-) {
-    override fun toString(): String =
-        """
-            Nome:       $nome
-            Salario:    $salario
-            
-        """.trimIndent()
-}
